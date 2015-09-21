62e490802cd4396a179089601451533dff3f9cf6
Ernesto De los Santos Cordero <ernestodlsc@gmail.com>
2015-09-21 17:59:02 -0400

    /groups route added.

--- routes/groups.js
+++ routes/groups.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
+/* GET group */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+   res.send('Groups');
 });
 
 module.exports = router;
